b4c49ad2513116d31289dc1b3f5e3aa40ceb32e4
Alexander Loboda <loboda@yahoo.com>
2023-02-21 00:13:52 -0800

    my changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,8 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">*Learn React*</a>
-          <h1 className="App-title">Fib Calculator</h1>
-         <Link to="/">Home</Link>
+          <h1 className="App-title">Fibonachi Numbers Calculator</h1>
+          <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
